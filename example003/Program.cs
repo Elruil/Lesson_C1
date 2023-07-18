be3c3b97d5b2dded2f1c81afb20fb825f1b52dd3
Artem <Elandriel@mail.ru>
2023-07-19 03:24:27 +0500

    example004

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 31;
-int numberB = 27;
+﻿int numberA = new Random().Next(1,100);
+int numberB = new Random().Next(1,100);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
