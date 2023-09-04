0254e828654fb9a3ca9995059ad036719b0fcc9e
Artem <Elandriel@mail.ru>
2023-09-04 17:44:39 +0500

    adding example041-043

---
+++ Example043/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+ //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+ Console.Clear();
+
+ double b1 = GetNumberFromUser("Введите целое число для определения значения b1", "Ошибка ввода");
+ double k1 = GetNumberFromUser("Введите целое число для определения значения k1", "Ошибка ввода");
+ double b2 = GetNumberFromUser("Введите целое число для определения значения b2", "Ошибка ввода");
+ double k2 = GetNumberFromUser("Введите целое число для определения значения k2", "Ошибка ввода");
+
+if (b1 == k1)
+{
+    Console.WriteLine("Прямые паралельны");
+}
+else
+{
+double x = (b2 - b1)/(k1 - k2);
+double y = (k1*(b2-b1))/(k1-k2)+b1;
+Console.WriteLine($"Введенные значения b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}. Точка пересечения ({x};{y}) ");
+}
+
+ double GetNumberFromUser(string Message, string errorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(Message);
+        bool isCorrect = double.TryParse(Console.ReadLine(), out double SizeFromUser);
+        if (isCorrect)
+        {
+            return SizeFromUser;
+        }
+        Console.WriteLine(errorMessage);
+    }
+}
--- example041/Program.cs
+++ example041/Program.cs
@@ -1,19 +1,47 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
 //Посчитайте, сколько чисел больше 0 ввёл пользователь.
 Console.Clear();
-int Number = GetNumberFromUser("Введите целое число", "Ошибка ввода");
-int[] Array = GetArray(Number);
+int Number = GetNumberFromUser("Введите целое число для определения длины массива", "Ошибка ввода");
+int[] Array = GetArray(Number, "Введите число для заполнения массива");
+
+Console.WriteLine($"длина массива {Number}");
+Console.WriteLine(String.Join(", ", Array));
+int Quantity =  GetQuantityEvenNumbers(Array);
+Console.WriteLine($" {Quantity} элемента массива больше нуля");
+
+int GetQuantityEvenNumbers(int[] Arr)
+{
+    int i = 0;
+    foreach (int el in Arr)
+    {
+    if (el > 0)
+    i++;
+    
+    }
+    return i;
+}
 
 
-
-static int[] GetArray(int size, int min, int max)
+static int[] GetArray(int size,string errorMessage)
 {
     int[] arr = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        Console.WriteLine(Message);
-        arr[i] = int.TryParse(Console.ReadLine());
-    }
+    for (int i = 0; i < arr.Length; i++)
+      
+        try
+        {
+            Console.WriteLine($"Введите {i} элемент массива");
+            arr[i] = int.Parse(Console.ReadLine() ?? "");
+        
+           
+        
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"{errorMessage} {exc.Message}");
+            i--;
+        }
+    
+
     return arr;
 }
 
