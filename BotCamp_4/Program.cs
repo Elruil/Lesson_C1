a0c58fa52ad75a4cd2181ae46b097c5b040a8b2b
Artem <Elandriel@mail.ru>
2023-09-24 20:40:22 +0500

    bootcamp_4

---
+++ BotCamp_4/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число элементов массива");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"число элементов массива {size}");
+int[] Array = new int[size];
+for (int i = 0; i < size; i++)
+{
+    Array[i] = new Random().Next(-20, 21);
+}
+Console.WriteLine($"Полученный массив - [{String.Join(' ', Array)}]");
+SortVibor(Array);
+Console.WriteLine($"Полученный массив - [{String.Join(' ', Array)}]");
+int[] SortVibor(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int IndexMin = i;
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[j] < array[IndexMin])
+            {
+                IndexMin = j;
+            }
+            if (array[IndexMin] == array[i])
+                continue;
+
+            int temp = array[i];
+            array[i] = array[IndexMin];
+            array[IndexMin] = temp;
+        }
+
+    }
+    return array;
+}
